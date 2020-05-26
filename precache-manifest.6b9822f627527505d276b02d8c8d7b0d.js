c65518cb9b675b478b2a2e33b540668908746b24
Amrendra Kumar <amarmca51@gmail.com>
2020-05-26 17:58:25 +0530

    Updates

---
+++ precache-manifest.6b9822f627527505d276b02d8c8d7b0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe1ed14b7e9e8c193081",
+    "url": "/ecommerce-shopping-cart/static/css/main.7f579559.chunk.css"
+  },
+  {
+    "revision": "fe1ed14b7e9e8c193081",
+    "url": "/ecommerce-shopping-cart/static/js/main.6a4e13ee.chunk.js"
+  },
+  {
+    "revision": "a0e27bc38c15e076523a",
+    "url": "/ecommerce-shopping-cart/static/js/runtime~main.e9daa8c9.js"
+  },
+  {
+    "revision": "6953d4ece5fea2727170",
+    "url": "/ecommerce-shopping-cart/static/js/2.3313198c.chunk.js"
+  },
+  {
+    "revision": "ade1672eff0998aac1363ef9adfd173f",
+    "url": "/ecommerce-shopping-cart/index.html"
+  }
+];
